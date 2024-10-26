3ffa9bd870143be529fee5731664b643dd1e8815
✰ sᴀᴛʏᴀ ʙᴀʙʏ ✰ <tridevk442@gmail.com>
2024-10-26 14:18:17 +0530

    Update play.py
    Signed-off-by: ✰ sᴀᴛʏᴀ ʙᴀʙʏ ✰ <tridevk442@gmail.com>

--- BABYMUSIC/plugins/play/play.py
+++ BABYMUSIC/plugins/play/play.py
@@ -633,7 +633,8 @@ async def slider_queries(client, CallbackQuery, _):
         title, duration_min, thumbnail, vidid = await YouTube.slider(query, query_type)
         buttons = slider_markup(_, vidid, user_id, query, query_type, cplay, fplay)
         # med = InputMediaPhoto(
-            media = thumbnail,
+            media = thumbnail
+        
             caption=_["play_10"].format(
                 title.title(),
                 duration_min,
