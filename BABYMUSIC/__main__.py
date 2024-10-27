7ae096804f1732760820693e3415a3470d8f761d
✰ sᴀᴛʏᴀ ʙᴀʙʏ ✰ <tridevk442@gmail.com>
2024-10-27 22:41:53 +0530

    Update __main__.py
    Signed-off-by: ✰ sᴀᴛʏᴀ ʙᴀʙʏ ✰ <tridevk442@gmail.com>

--- BABYMUSIC/__main__.py
+++ BABYMUSIC/__main__.py
@@ -71,6 +71,9 @@ async def start_server():
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(init())
-    loop.run_until_complete(start_server())
-    loop.run_forever()
+    try:
+        loop.run_until_complete(init())
+        loop.run_until_complete(start_server())
+        loop.run_forever()
+    except Exception as e:
+        LOGGER(__name__).error(f"Error in main loop: {e}")
