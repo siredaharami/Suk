dbe41b0261f185b092cc0f19ea2031cd27a7de69
✰ sᴀᴛʏᴀ ʙᴀʙʏ ✰ <tridevk442@gmail.com>
2024-10-27 09:15:14 +0530

    Update start.py
    Signed-off-by: ✰ sᴀᴛʏᴀ ʙᴀʙʏ ✰ <tridevk442@gmail.com>

--- BABYMUSIC/plugins/bot/start.py
+++ BABYMUSIC/plugins/bot/start.py
@@ -115,8 +115,7 @@ async def start_gp(client, message: Message, _):
     out = start_panel(_)
     uptime = int(time.time() - _boot_)
     await message.reply_photo(
-        random.choice(YUMI_PICS),
-        caption=_["start_1"].format(app.mention, get_readable_time(uptime)),
+        text=_["start_1"].format(app.mention, get_readable_time(uptime)),
         reply_markup=InlineKeyboardMarkup(out),
     )
     return await add_served_chat(message.chat.id)
