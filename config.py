afc8d21009647ba8c5273585f45e1053fce555db
✰ sᴀᴛʏᴀ ʙᴀʙʏ ✰ <tridevk442@gmail.com>
2024-10-27 20:21:25 +0530

    Update config.py
    Signed-off-by: ✰ sᴀᴛʏᴀ ʙᴀʙʏ ✰ <tridevk442@gmail.com>

--- config.py
+++ config.py
@@ -53,7 +53,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/BABY-MUSIC/BABYXMUSIC",
+    "https://github.com/BABY-MUSIC/SPOTIFY_MUSIC",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
