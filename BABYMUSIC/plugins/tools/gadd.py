ef90e52a33b01e014652bca3fccf2631e8e2b5ff
✰ sᴀᴛʏᴀ ʙᴀʙʏ ✰ <tridevk442@gmail.com>
2024-11-07 09:54:05 +0530

    Update gadd.py
    Signed-off-by: ✰ sᴀᴛʏᴀ ʙᴀʙʏ ✰ <tridevk442@gmail.com>

--- BABYMUSIC/plugins/tools/gadd.py
+++ BABYMUSIC/plugins/tools/gadd.py
@@ -3,8 +3,8 @@ import asyncio
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 OWNERS = "7131351568"
-from SONALI import app
-from SONALI.utils.database import add_served_chat, get_assistant
+from BABYMUSIC import app
+from BABYMUSIC.utils.database import add_served_chat, get_assistant
 
 
 @app.on_message(filters.command("gadd") & filters.user(int(OWNERS)))
